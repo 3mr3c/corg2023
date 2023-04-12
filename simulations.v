5b3ba69172c895751e82b13888ef71ac3deaf230
Emre Çelik <73967717+ituitis20-celikem19@users.noreply.github.com>
2023-04-12 17:10:53 +0300

    new changes

--- simulations.v
+++ simulations.v
@@ -1,21 +1,126 @@
 `timescale 1ns/1ps
 
+//PART 1
+
 module Register_Test();
+    reg En;
+    reg CLK;
+    reg [1:0] FunSel;
+    reg [7:0] I;
+    
+    wire [7:0] Q;
+
+    reg [1:0] e = 1'b0;
+    reg [1:0] f = 2'b00;
+     
+    reg [7:0] i = 8'b00000000;
+     
+    n_bitRegister #(.N(8)) R1(.E(En),.CLK(CLK), .FunSel(FunSel), .I(I), .Q(Q));
+    always #1 CLK = ~CLK;
+    always #100 En = ~En;
+    
+    initial begin
+    I = 8'b10101010;
+    En = 1;
+    CLK = 0;   
+    forever
+        begin 
+        #5
+        for(f = 0; f<3'b100; f = f+1) begin
+        #10
+        FunSel = f;
+        $display("FunSel1:%0d ", f );
+        end
+        $finish;  
+        end
+    end
   
 endmodule
+
+//PART 2
+
+//Part 2a
 
 module IR_Test();
-  
+    reg CLK;
+    reg E;
+    reg LH;
+
+    reg [1:0] FunSelect;
+    reg [7:0] Input;
+    
+    wire [15:0] Out;
+    
+    integer i;
+    
+    IR Ir(.LH(LH),.En(E),.FunSel(FunSelect),.I(Input),.CLK(CLK), .IRout(Out));
+    always #5 CLK = ~CLK;
+    always #50 E = ~E;
+    always #100 LH = ~LH;
+    
+    initial begin
+         Input = 8'b10101010;
+         E = 1;
+         CLK = 0;
+         LH=0;
+         forever begin
+            #5
+            for(i=0; i < 4; i = i + 1) begin
+                #10
+                FunSelect = i;
+                $display("FunSelect: %0d",i);
+            end
+         end    
+    end
 endmodule
 
+//Part 2b
+
 module RegFile_Test();
-  
+    reg CLK;
+    reg [1:0] FunSelect;
+    reg [7:0] Input;
+    reg [2:0] Out1Select;
+    reg [2:0] Out2Select;
+    reg [3:0] RSelect;
+    reg [3:0] TSelect;
+    
+    wire [7:0] Output1;
+    wire [7:0] Output2;
+    
+    integer a,j,k;
+    RegFile file(.O1Sel(Out1Select),.O2Sel(Out2Select),.FunSel(FunSelect),.RegSel(RSelect),.TSel(TSelect),.I(Input),.CLK(CLK),.O1(Output1),.O2(Output2));
+    always #1 CLK = ~CLK;
+    
+     initial begin
+     #5
+     Input = 8'b10101010;
+     CLK = 0;
+     for(a=0;a<16;a=a+1) begin
+        RegisterSelect = a;
+        $display("RegisterSelect: %0d",a);
+        for(j=0;j<4;j=j+1) begin
+            #10
+            FunSelect = j;
+            $display("FunSelect: %0d ", j);
+            for(k = 0; k < 4; k = k+1)begin
+                #5
+                OutASelect = k;
+                OutBSelect = k;
+                $display("Out1 and Out2 Select: %0d",k);
+            end
+        end
+     end     
+     end
 endmodule
+
+//Part 2c
 
 module ARF_Test();
   
 endmodule
 
+//PART 3
 
 module ALU_test();
   
